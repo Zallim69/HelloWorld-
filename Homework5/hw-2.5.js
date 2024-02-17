3a3688241aa1853de845eec3a754632a08089803
Zallim69 <156121312+Zallim69@users.noreply.github.com>
2024-02-17 23:08:33 +0300

    Сделал)

--- Homework5/hw-2.5.js
+++ Homework5/hw-2.5.js
@@ -1,19 +1,32 @@
- // Задание 1
-    function findMin(num1, num2) {
-  if (num1 < num2) {
-    return num1;
+// Задание 1
+const findMin = (a, b) => {
+  if (a < b) {
+    return a;
   } else {
-    return num2;
+    return b;
   }
 }
 
-var number1 = 8;
-var number2 = 4;
-var result = findMin(number1, number2);
-alert(result);
+console.log(findMin(8, 4));
+console.log(findMin(6, 6));
 
-    // Задание 2
-    function checkEvenOdd(n) {
+/*первое задание с плавающими вводными*/
+const compareNumbers = () => {
+  let num1 = Number(prompt("Введите первое число"));
+  let num2 = Number(prompt("Введите второе число"));
+  if (num1 > num2) {
+    alert("Первое число больше");
+  } else if (num1 < num2) {
+    alert("Второе число больше");
+  } else {
+    alert("Числа равны");
+  }
+}
+
+compareNumbers();
+
+// Задание 2
+const checkEvenOdd = (n) => {
   if (n % 2 === 0) {
     return 'Число четное';
   } else {
@@ -21,28 +34,28 @@ alert(result);
   }
 }
 
-    // Задание 3.1
-    function printSquare(number) {
-  var square = number * number;
+// Задание 3.1
+const printSquare = (number) => {
+  const square = number * number;
   alert("Квадрат числа " + number + " равен " + square);
 }
 
-var number = prompt("Введите число:");
+const number = prompt("Введите число:");
 printSquare(number);
-   
+
 // Задание 3.2
-function squareNumber(number) {
+const squareNumber = (number) => {
   return number * number;
 }
 
-var inputNumber = prompt("Введите число:");
-var result = squareNumber(inputNumber);
+const inputNumber = prompt("Введите число:");
+const result = squareNumber(inputNumber);
 
 alert("Квадрат числа " + inputNumber + " равен " + result);
 
-    // Задание 4
-    function checkAge() {
-  var age = prompt("Сколько Вам лет?");
+// Задание 4
+const checkAge = () => {
+  const age = prompt("Сколько Вам лет?");
   
   if (age < 0) {
     alert("Вы ввели неправильное значение");
@@ -55,8 +68,8 @@ alert("Квадрат числа " + inputNumber + " равен " + result);
 
 checkAge();
 
-    // Задание 5
-    function multiplyNumbers(num1, num2) {
+// Задание 5
+const multiplyNumbers = (num1, num2) => {
   if (isNaN(num1) || isNaN(num2)) {
     return 'Одно или оба значения не являются числом';
   }
@@ -66,55 +79,51 @@ checkAge();
 
 alert(multiplyNumbers(5, 10)); // Выведет 50
 alert(multiplyNumbers('abc', 10)); // Выведет 'Одно или оба значения не являются числом'
- 
-    // Задание 6
-    function cubeNumber() {
-  var input = prompt("Введите число:");
+
+// Задание 6
+const cubeNumber = () => {
+  const input = prompt("Введите число:");
   
   if (isNaN(input)) {
     alert("Переданный параметр не является числом");
     return;
   }
   
-  var number = Number(input);
-  var result = number ** 3;
-  
+  const number = Number(input);
+  const result = number ** 3;
   alert(number + " в кубе равняется " + result);
 }
 
 cubeNumber();
 
-    // Задание 7
-    // Создаем объект circle1
-var circle1 = {
-  radius: 5, // задаем радиус
+// Задание 7
+const circle1 = {
+  radius: 5,
   getArea: function() {
-    return Math.PI * this.radius * this.radius; // выисляем площадь круга
+    return Math.PI * this.radius * this.radius;
   },
   getPerimeter: function() {
-    return 2 * Math.PI * this.radius; // вычисляем периметр окружности
+    return 2 * Math.PI * this.radius;
   }
 };
 
-// Создаем объект circle2
-var circle2 = {
-  radius: 10, // задаем радиус
+const circle2 = {
+  radius: 10,
   getArea: function() {
-    return Math.PI * this.radius * this.radius; // вычисляем площадь круга
+    return Math.PI * this.radius * this.radius;
   },
   getPerimeter: function() {
-    return 2 * Math.PI * this.radius; // вычисляем периметр окружности
+    return 2 * Math.PI * this.radius;
   }
 };
 
-// Выводим результаты через alert
 alert("Площадь круга circle1: " + circle1.getArea());
 alert("Периметр окружности circle1: " + circle1.getPerimeter());
 alert("Площадь круга circle2: " + circle2.getArea());
 alert("Периметр окружности circle2: " + circle2.getPerimeter());
 
-    // Задание 8
-    function getSeason(month) {
+// Задание 8
+const getSeason = (month) => {
   if (month === 12 || month === 1 || month === 2) {
     return "Зима";
   } else if (month === 3 || month === 4 || month === 5) {
